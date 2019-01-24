32483b23d9e9e6d9da213422fa296fc25ca31d11
Nielene Thomas <nielenethomas@pursuit.org>
2019-01-24 18:01:04 -0500

    first commit

---
+++ expand_blog/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+import { Feed } from './components/Feed.js'
+import blog_posts from './blog_posts.json'
+
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      allPosts: blog_posts,
+    }
+    // debugger
+  }
+
+
+  render() {
+    return (
+      <div className='App'>
+        <Feed allPosts={this.state.allPosts} />
+      </div>
+    )
+  }
+}
+
+export default App
